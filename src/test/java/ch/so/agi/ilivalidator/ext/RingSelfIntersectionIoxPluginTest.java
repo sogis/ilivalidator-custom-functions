c83db1bc912dbc6eebbf6b990dab0129e7f3271e
edigonzales <edi.gonzales@gmail.com>
2021-03-15 18:36:48 +0100

    start with ring self intersection

---
+++ src/test/java/ch/so/agi/ilivalidator/ext/RingSelfIntersectionIoxPluginTest.java
@@ -0,0 +1,40 @@
+package ch.so.agi.ilivalidator.ext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import ch.interlis.ili2c.config.Configuration;
+import ch.interlis.ili2c.config.FileEntry;
+import ch.interlis.ili2c.config.FileEntryKind;
+import ch.interlis.ili2c.metamodel.TransferDescription;
+
+public class RingSelfIntersectionIoxPluginTest {
+    private TransferDescription td = null;
+    private final static String OBJ_OID1 ="o1";
+    private final static String ILI_TOPIC = "Testmodel.Topic";
+    private final static String ILI_CLASSE = ILI_TOPIC+".ClassE";
+    private final static String BID1 = "b1";
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        Configuration ili2cConfig = new Configuration();
+        {
+            FileEntry fileEntry = new FileEntry("src/test/data/SO_FunctionsExt.ili", FileEntryKind.ILIMODELFILE);
+            ili2cConfig.addFileEntry(fileEntry);
+        }
+        {
+            FileEntry fileEntry = new FileEntry("src/test/data/Testmodel.ili", FileEntryKind.ILIMODELFILE);
+            ili2cConfig.addFileEntry(fileEntry);
+        }
+        td = ch.interlis.ili2c.Ili2c.runCompiler(ili2cConfig);
+        assertNotNull(td);
+    }
+    
+    @Test
+    public void findRingSelfIntersection() {
+        
+    }
+
+}
